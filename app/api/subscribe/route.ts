8fcad159967e77587fc4540e5aa7f1df59dc1e7c
hanbu97 <hanbu97@proton.me>
2023-11-06 13:07:49 +0800

    feat: web2 login

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,10 +1,6 @@
 import * as z from 'zod';
 import { emailSchema } from '@/lib/validation/contact';
 
-const FORM_ID = process.env.CONVERTKIT_FORM_ID;
-const API_KEY = process.env.CONVERTKIT_API_KEY;
-const API_URL = process.env.CONVERTKIT_API_URL;
-
 export async function POST(req: Request, res: Response) {
     try {
         // Get the request body and validate it.
@@ -13,16 +9,16 @@ export async function POST(req: Request, res: Response) {
         const email = body.email;
 
         //what do we want to send to CK?
-        const data = { email, api_key: API_KEY };
+        // const data = { email, api_key: API_KEY };
 
         // Update the post.
         // TODO: Implement sanitization for content.
         // ship it :)
-        const response = await fetch(`${API_URL}forms/${FORM_ID}/subscribe`, {
-            body: JSON.stringify(data),
-            headers: { 'Content-Type': 'application/json' },
-            method: 'POST',
-        });
+        // const response = await fetch(`${API_URL}forms/${FORM_ID}/subscribe`, {
+        //     body: JSON.stringify(data),
+        //     headers: { 'Content-Type': 'application/json' },
+        //     method: 'POST',
+        // });
 
         return new Response(null, { status: 200 });
     } catch (error) {
