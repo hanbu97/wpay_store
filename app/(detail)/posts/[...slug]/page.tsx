ebbf049af2cf05f8e4bef89dc0fb2d181f97cd57
hanbu97 <hanbu97@proton.me>
2023-11-12 12:41:35 +0800

    todo: add metamask

--- app/(detail)/posts/[...slug]/page.tsx
+++ app/(detail)/posts/[...slug]/page.tsx
@@ -21,6 +21,7 @@ import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
 import { MetaMaskButton } from "@metamask/sdk-react-ui";
 import { MetamaskProvider } from "@/hooks/useMetamask";
+import DetailPostSignInToPurchase from "@/components/detail/post/comment/detail-post-sign-in-to-purchase";
 
 export const revalidate = 0;
 
@@ -174,14 +175,17 @@ export default async function PostPage({ params }: PostPageProps) {
               </div>
             </div>
           </div>
-          <Button
+          {/* <Button
             type="button"
             className="group mt-8 flex flex-col w-full rounded-lg bg-gradient-to-t from-gray-200 via-gray-100 to-gray-50 p-2 text-center text-gray-400 shadow-md shadow-black/5 ring-1 ring-black/10 transition duration-200 hover:bg-gradient-to-tr hover:from-gray-200 hover:via-gray-100 hover:to-gray-50 active:scale-[96%] active:ring-black/20"
               >
                 <span className="text-gray-600 text-lg font-bold">
                   Place Order
                 </span>
-          </Button>
+          </Button> */}
+          <DetailPostSignInToPurchase />
+
+          
           {/* <MetamaskProvider> */}
               {/* <MetaMaskButton theme={"light"} color="white"></MetaMaskButton> */}
           {/* </MetamaskProvider> */}
